4de996082ce1f1003bc1dde2407d598ba8dbdd96
Juan_Luis_Trincado <juanluis.trincado@upf.edu>
2017-01-13 14:26:31 +0100

    Modified fix_coordinates.py

--- fix_coordinates.py
+++ fix_coordinates.py
@@ -8,13 +8,15 @@ The user must provide the position in the file of this two columns (first column
 """
 
 import pandas as pd
-import sys
+import sys, time
 
 try:
 
+    print("Starting execution: " + time.strftime('%H:%M:%S'))
+
     file_path = sys.argv[1]
-    pos_start = sys.argv[2]+1
-    pos_end = sys.argv[3]+1
+    pos_start = int(sys.argv[2])+1
+    pos_end = int(sys.argv[3])+1
     output_path = sys.argv[4]
 
     # file_path = "/projects_rg/Annotation/Junctions/test.tab"
@@ -23,6 +25,7 @@ try:
     # output_path = "/projects_rg/Annotation/Junctions/test2.tab"
 
     # Load the file
+    print("Loading file "+file_path)
     file = pd.read_table(file_path, delimiter="\t")
 
     # Transform to int the start and end coordinates
@@ -30,7 +33,11 @@ try:
     file.ix[:, pos_end] = file.ix[:, pos_end].astype(int)
 
     #Save it
+    print("Saving file "+output_path)
     file.to_csv(output_path, sep="\t", index=False)
+
+    print("Done. Exiting program. " + time.strftime('%H:%M:%S') + "\n\n")
+    exit(0)
 
 except Exception as error:
     print('\nERROR: ' + repr(error))
