5b5c55802d37f523c30e7040f3260f5c242f7161
Juan_Luis_Trincado <juanluis.trincado@upf.edu>
2017-01-16 18:40:47 +0100

    Modified fix_coordinates.py

--- fix_coordinates.py
+++ fix_coordinates.py
@@ -2,12 +2,11 @@
 @authors: Juan L. Trincado
 @email: juanluis.trincado@upf.edu
 
-fix_coordinates.py: Takes any file and transform the start and end coordinates it to integer
-The user must provide the position in the file of this two columns (first column will be 1)
+fix_coordinates.py: Takes any file and transform the id, start and end coordinates it to integer
+The user must provide the position in the file of this three columns (first column will be 1)
 
 """
 
-import pandas as pd
 import sys, time
 
 try:
@@ -15,26 +14,37 @@ try:
     print("Starting execution: " + time.strftime('%H:%M:%S'))
 
     file_path = sys.argv[1]
-    pos_start = int(sys.argv[2])+1
-    pos_end = int(sys.argv[3])+1
-    output_path = sys.argv[4]
+    pos_id = int(sys.argv[2])-1
+    pos_start = int(sys.argv[3])-1
+    pos_end = int(sys.argv[4])-1
+    output_path = sys.argv[5]
 
     # file_path = "/projects_rg/Annotation/Junctions/test.tab"
     # pos_start = 2
     # pos_end = 3
     # output_path = "/projects_rg/Annotation/Junctions/test2.tab"
 
+    print("Formatting file " + file_path + "...")
+
+    outFile = open(output_path, 'w')
+
     # Load the file
-    print("Loading file "+file_path)
-    file = pd.read_table(file_path, delimiter="\t")
+    with open(file_path) as f:
+        outFile.write(next(f))
+        for line in f:
+            tokens = line.rstrip().split("\t")
+            id = tokens[pos_id].split(";")
+            id[1] = str(int(float(id[1])))
+            id[2] = str(int(float(id[2])))
+            tokens[pos_id] = ";".join(id)
+            tokens[pos_start] = str(int(float(tokens[pos_start])))
+            tokens[pos_end] = str(int(float(tokens[pos_end])))
+            new_line = "\t".join(tokens)
+            outFile.write(new_line + "\n")
 
-    # Transform to int the start and end coordinates
-    file.ix[:, pos_start] = file.ix[:, pos_start].astype(int)
-    file.ix[:, pos_end] = file.ix[:, pos_end].astype(int)
+    outFile.close()
 
-    #Save it
-    print("Saving file "+output_path)
-    file.to_csv(output_path, sep="\t", index=False)
+    print("Saved " + output_path)
 
     print("Done. Exiting program. " + time.strftime('%H:%M:%S') + "\n\n")
     exit(0)
