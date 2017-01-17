6cbe90d7f96783b1b81c370112f0e61de282dac9
Juan_Luis_Trincado <juanluis.trincado@upf.edu>
2017-01-17 16:09:46 +0100

    Fixed bugs STARtoBED.R

--- STARtoBED.R
+++ STARtoBED.R
@@ -10,16 +10,18 @@ require(data.table)
 CHARACTER_command_args <- commandArgs(trailingOnly=TRUE)
 
 sj_count <- fread(file=CHARACTER_command_args[1],sep="\t")
+# sj_count <- fread(file="/projects_rg/Bellmunt/STAR/whole_cohort_v2/20160527-vh100-pool5-CWZ3082-1/SJ.out.tab",sep="\t")
 
 colnames(sj_count) <- c("chrom","first_bp_intron","last_bp_intron","strand","intron_motif"
                     ,"annotated","unique_junction_reads","multimap_junction_reads"
                     ,"max_overhang")
 #Don't forget to print the number without the scientifc notation (i.e. 2.7e+13)
-sj_count$first_bp_intron <- as.character(format(sj_count$first_bp_intron - 2, scientific = FALSE))
-sj_count$last_bp_intron <- as.character(format(sj_count$last_bp_intron + 1, scientific = FALSE))
+sj_count$first_bp_intron <- gsub("\\s","",as.character(format(sj_count$first_bp_intron - 2, scientific = FALSE)))
+sj_count$last_bp_intron <- gsub("\\s","",as.character(format(sj_count$last_bp_intron + 1, scientific = FALSE)))
 sj_count$strand2 <- ifelse(sj_count$strand=="1","+",ifelse(sj_count$strand=="2","-",NA))
 sj_count$score <- 0
-sj_count$id <- paste0(as.character(sj_count$chrom),";",gsub("\\s","",sj_count$first_bp_intron),";",gsub("\\s","",sj_count$last_bp_intron),";",sj_count$strand2)
+#There are some blank spaces in the fields. Remove it with gsub
+sj_count$id <- paste0(gsub("\\s","",as.character(sj_count$chrom)),";",gsub("\\s","",sj_count$first_bp_intron),";",gsub("\\s","",sj_count$last_bp_intron),";",sj_count$strand2)
 #Remove the ones without strand
 sj_count_filter <- sj_count[which(!is.na(sj_count$strand2)),]
 sj_count_filter2 <- sj_count_filter[,c("chrom","first_bp_intron","last_bp_intron","id","unique_junction_reads","strand2","annotated")]
