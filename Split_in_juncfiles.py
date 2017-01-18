589e8284ef5cfa12bdef38dc6a9e226de3a1dcb5
Juan_Luis_Trincado <juanluis.trincado@upf.edu>
2017-01-18 15:02:42 +0100

    Modified Split_in_juncfiles.py

--- Split_in_juncfiles.py
+++ Split_in_juncfiles.py
@@ -1,34 +1,42 @@
-#This script take the junctions with the read counts and generate a .junc file per column
+"""
+@authors: Juan L. Trincado
+@email: juanluis.trincado@upf.edu
 
-#arg[1]: path to the STAR output file (one folder per sample)
+Split_in_juncfiles.py: Takes the file with the read counts of the junctions and all the samples
+and generates a .junc file per sample for running LeafCutter
 
-__author__ = 'juanluis'
+"""
 
 import sys
 import pandas as pd
-import os
-import shlex
 import time
-from copy import deepcopy
+import os
 
 def main():
     try:
 
+        readCounts_path = sys.argv[1]
+        # readCounts_path = "/projects_rg/Annotation/Junctions/test.tab"
+
         # 1. Load the junction file with pandas
         print("Loading phenotype...")
-        junctions = pd.read_table(sys.argv[1], delimiter="\t")
-        path = sys.argv[1].split("/")
+        readCounts_file = pd.read_table(readCounts_path, delimiter="\t")
+        path = readCounts_path.split("/")
         del path[-1]
-        path2 = "/".join(path)
+        path2 = "/".join(path)+"/Junc_files"
 
         # 2. Per column, we will create a data frame and save it
+        #Create the directory, if it doesn't exist
+        if not os.path.exists(path2):
+            os.makedirs(path2)
         # Open a file for putting the location of each file
         path_index = path2 + "/index_juncfiles.txt"
         index_file = open(path_index, "w")
-        for i in range(8,len(junctions.columns)):
+        for i in range(8,len(readCounts_file.columns)):
+            # print(i)
             list = [1,2,3,4]
             list.insert(3, i)
-            aux_df = junctions.iloc[:,list]
+            aux_df = readCounts_file.iloc[:,list]
 
             #Add an extra column with dots and resort the columns
             aux_df['aux'] = "."
@@ -36,9 +44,9 @@ def main():
             cols2 = cols[0:3] + cols[5].split(" ") + cols[3:5]
             aux_df = aux_df[cols2]
 
-            idSample = junctions.columns.values[i]
+            idSample = readCounts_file.columns.values[i]
             nameFile = path2 + "/" + idSample + ".junc"
-            # nameFile = "/projects_rg/Annotation/Junctions/Junc_files/" + idSample + ".junc"
+
             #Save the dataframe
             print("Creating files "+nameFile+"...")
             aux_df.to_csv(nameFile, sep="\t", index=False,  float_format='%.f', header=False)
@@ -50,7 +58,9 @@ def main():
 
         exit(0)
 
-    except Exception:
+    except Exception as error:
+        print('\nERROR: ' + repr(error))
+        print("Aborting execution")
         sys.exit(1)
 
 
